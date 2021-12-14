9b31351bf3e102a5e297db59bb32a64b0d5ec872
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-14 11:08:17 +0700

    scan page

---
+++ lib/screens/scan_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatefulWidget {
+  const ScanPage({Key? key}) : super(key: key);
+
+  @override
+  _ScanPageState createState() => _ScanPageState();
+}
+
+class _ScanPageState extends State<ScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("สแกน"),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
